4208cbff6c7ace36cbaab7cf4143c64ae072f8aa
Rubi Henjaya <rubi.henjaya@gmail.com>
2022-07-06 14:49:49 +0700

    database connected

--- app.js
+++ app.js
@@ -3,18 +3,131 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
-var app = express();
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Database Name
+const dbName = 'breadsdb';
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+async function connect() {
+    // Use connect method to connect to the server
+    try {
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
 
-module.exports = app;
+        return db
+
+    } catch (err) {
+        throw "gagal bro"
+    }
+}
+
+connect()
+    .then((db) => {
+        var indexRouter = require('./routes/index')(db);
+        var usersRouter = require('./routes/users')(db);
+
+        var app = express();
+
+        app.use(logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: false }));
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, 'public')));
+
+        app.use('/', indexRouter);
+        app.use('/users', usersRouter);
+
+        var debug = require('debug')('cobaapi:server');
+        var http = require('http');
+
+        /**
+         * Get port from environment and store in Express.
+         */
+
+        var port = normalizePort(process.env.PORT || '3000');
+        app.set('port', port);
+
+        /**
+         * Create HTTP server.
+         */
+
+        var server = http.createServer(app);
+
+        /**
+         * Listen on provided port, on all network interfaces.
+         */
+
+        server.listen(port);
+        server.on('error', onError);
+        server.on('listening', onListening);
+
+        /**
+         * Normalize a port into a number, string, or false.
+         */
+
+        function normalizePort(val) {
+            var port = parseInt(val, 10);
+
+            if (isNaN(port)) {
+                // named pipe
+                return val;
+            }
+
+            if (port >= 0) {
+                // port number
+                return port;
+            }
+
+            return false;
+        }
+
+        /**
+         * Event listener for HTTP server "error" event.
+         */
+
+        function onError(error) {
+            if (error.syscall !== 'listen') {
+                throw error;
+            }
+
+            var bind = typeof port === 'string'
+                ? 'Pipe ' + port
+                : 'Port ' + port;
+
+            // handle specific listen errors with friendly messages
+            switch (error.code) {
+                case 'EACCES':
+                    console.error(bind + ' requires elevated privileges');
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    console.error(bind + ' is already in use');
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        }
+
+        /**
+         * Event listener for HTTP server "listening" event.
+         */
+
+        function onListening() {
+            var addr = server.address();
+            var bind = typeof addr === 'string'
+                ? 'pipe ' + addr
+                : 'port ' + addr.port;
+            debug('Listening on ' + bind);
+        }
+    })
+    .catch(console.error)
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
+  router.get('/', function (req, res, next) {
+    const collection = db.collection('documents');
+    res.render('index', { title: 'Express' });
+  });
 
-module.exports = router;
+  return router;
+}
