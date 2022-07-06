70d1115c6657285f7534650cd8c89d5482212f6f
Rubi Henjaya <rubi.henjaya@gmail.com>
2022-07-06 17:14:02 +0700

    breads done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,92 @@
 var express = require('express');
 var router = express.Router();
+const mongodb = require('mongodb')
 
 module.exports = function (db) {
-  router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+  router.get('/', async function (req, res, next) {
+    const page = req.query.page || 'all'
+    const limit = 2
+    const offset = page == 'all' ? 0 : (page - 1) * limit
+    const searchParams = {}
+
+    // browse
+    if (req.query.nama) {
+      const regexName = new RegExp(`${req.query.nama}`, 'i');
+      searchParams['nama'] = regexName
+    }
+
+    if (req.query.sudahMenikah) {
+      searchParams['sudahMenikah'] = JSON.parse(req.query.sudahMenikah)
+    }
+
+    try {
+      const collection = db.collection('users');
+
+      const totalData = await collection.find(searchParams).count()
+      const totalPages = page == 'all' ? 1 : Math.ceil(totalData / limit)
+      const limitation = page == 'all' ? {} : { limit, skip: offset }
+
+      const users = await collection.find(searchParams, limitation).toArray();
+      res.status(200).json({
+        data: users,
+        totalData,
+        totalPages,
+        display: limit,
+        currentPage: page == 'all' ? 'all' : parseInt(page)
+      })
+    } catch (err) {
+      res.status(500).json({ message: "error ambil data" })
+    }
+  });
+
+  router.post('/', async function (req, res, next) {
+    try {
+      const collection = db.collection('users');
+      const returnDocument = await collection.insertOne({
+        nama: req.body.nama,
+        tinggi: parseFloat(req.body.tinggi),
+        sudahMenikah: JSON.parse(req.body.sudahMenikah)
+      });
+      const user = await collection.findOne({ _id: returnDocument.insertedId })
+      res.status(200).json(user)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: "error tambah data" })
+    }
+  });
+
+  router.put('/:id', async function (req, res, next) {
+    try {
+      const collection = db.collection('users');
+      await collection.updateOne({
+        _id: mongodb.ObjectId(req.params.id)
+      }, {
+        $set: {
+          nama: req.body.nama,
+          tinggi: parseFloat(req.body.tinggi),
+          sudahMenikah: JSON.parse(req.body.sudahMenikah)
+        }
+      });
+      const user = await collection.findOne({ _id: mongodb.ObjectId(req.params.id) })
+      res.status(200).json(user)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: "error update data" })
+    }
+  });
+
+  router.delete('/:id', async function (req, res, next) {
+    try {
+      const collection = db.collection('users');
+      const user = await collection.findOne({ _id: mongodb.ObjectId(req.params.id) })
+      await collection.deleteMany({
+        _id: mongodb.ObjectId(req.params.id)
+      });
+      res.status(200).json(user)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ message: "error delete data" })
+    }
   });
 
   return router;
